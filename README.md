11c3e316b23da6316d8c745afcfe9c0d9c560025
ed cuss <edcussmusic@gmail.com>
2025-10-06 20:05:01 +0100

    fix: annotate kwargs

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # polars-pipe
+
+
+# Repo map
+```
+├── .github
+│   └── workflows
+│       └── ci_tests.yaml
+├── src
+│   └── polars_pipe
+│       ├── adapters
+│       │   ├── __init__.py
+│       │   └── io_pl.py
+│       ├── core
+│       │   ├── __init__.py
+│       │   └── logger.py
+│       ├── services
+│       │   └── __init__.py
+│       ├── __init__.py
+│       └── __main__.py
+├── tests
+│   └── polars_pipe
+│       ├── adapters
+│       │   └── __init__.py
+│       ├── core
+│       │   └── __init__.py
+│       ├── services
+│       │   └── __init__.py
+│       ├── __init__.py
+│       └── test_main.py
+├── .pre-commit-config.yaml
+├── README.md
+├── pyproject.toml
+├── ruff.toml
+└── uv.lock
+::
+```
--- src/polars_pipe/adapters/io_pl.py
+++ src/polars_pipe/adapters/io_pl.py
@@ -1,12 +1,16 @@
+from enum import Enum, auto
+
 import attrs
 import polars as pl
-from enum import Enum, auto
+
 from polars_pipe.core.logger import logger
+
 
 class FileType(Enum):
     JSON = auto()
     PARQUET = auto()
     CSV = auto()
+
 
 READ_FUNCS = {
     FileType.JSON: pl.read_json,
@@ -15,25 +19,29 @@ READ_FUNCS = {
 }
 
 
-def write_parquet(df: pl.DataFrame, path: str, **kwargs) -> None:
+def write_parquet(df: pl.DataFrame, path: str, **kwargs: dict) -> None:
     df.write_parquet(path, **kwargs)
+
 
 WRITE_FUNCS = {
     FileType.PARQUET: write_parquet,
 }
 
+
 @attrs.define
 class IOWrapper:
     _read_funcs: dict = attrs.field(default=READ_FUNCS)
     _write_funcs: dict = attrs.field(default=WRITE_FUNCS)
-    
-    def read(self, path: str, file_type: FileType, **kwargs) -> pl.DataFrame:
+
+    def read(self, path: str, file_type: FileType, **kwargs: dict) -> pl.DataFrame:
         logger.debug(f"{path = } {file_type = } {kwargs = }")
         if file_type not in self._read_funcs:
             raise NotImplementedError(f"`read` is not implemented for {file_type}")
         return self._read_funcs[file_type](path, **kwargs)
-    
-    def write(self, df: pl.DataFrame, path: str, file_type: FileType, **kwargs) -> pl.DataFrame:
+
+    def write(
+        self, df: pl.DataFrame, path: str, file_type: FileType, **kwargs: dict
+    ) -> pl.DataFrame:
         logger.debug(f"{path = } {file_type = } {kwargs = }")
         if file_type not in self._write_funcs:
             raise NotImplementedError(f"`write` is not implemented for {file_type}")
