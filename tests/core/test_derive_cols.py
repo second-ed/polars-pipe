4fb66a3e3a95ac5be48b82b1fbf2c057f811229a
ed cuss <edcussmusic@gmail.com>
2025-10-16 21:50:07 +1300

    fix: simplify test

--- tests/core/test_derive_cols.py
+++ tests/core/test_derive_cols.py
@@ -13,62 +13,24 @@ BASIC_DF = pl.DataFrame(
 
 
 @pytest.mark.parametrize(
-    ("raw_data", "derive_fn", "cols", "expected_result"),
+    ("raw_data", "derive_fn", "cols", "expected_series_values"),
     [
-        pytest.param(
-            BASIC_DF,
-            dc.add_cols,
-            ["a", "a", "a"],
-            BASIC_DF.hstack([pl.Series("result", [3, 3, 3])]),
-        ),
-        pytest.param(
-            BASIC_DF, dc.add_cols, ["a", "b"], BASIC_DF.hstack([pl.Series("result", [3, 3, 3])])
-        ),
-        pytest.param(
-            BASIC_DF, dc.add_cols, ["b", "b"], BASIC_DF.hstack([pl.Series("result", [4, 4, 4])])
-        ),
-        pytest.param(
-            BASIC_DF,
-            dc.sub_cols,
-            ["a", "a", "a"],
-            BASIC_DF.hstack([pl.Series("result", [-1, -1, -1])]),
-        ),
-        pytest.param(
-            BASIC_DF, dc.sub_cols, ["a", "b"], BASIC_DF.hstack([pl.Series("result", [-1, -1, -1])])
-        ),
-        pytest.param(
-            BASIC_DF, dc.sub_cols, ["b", "b"], BASIC_DF.hstack([pl.Series("result", [0, 0, 0])])
-        ),
-        pytest.param(
-            BASIC_DF,
-            dc.mul_cols,
-            ["a", "a", "a"],
-            BASIC_DF.hstack([pl.Series("result", [1, 1, 1])]),
-        ),
-        pytest.param(
-            BASIC_DF, dc.mul_cols, ["a", "b"], BASIC_DF.hstack([pl.Series("result", [2, 2, 2])])
-        ),
-        pytest.param(
-            BASIC_DF, dc.mul_cols, ["b", "b"], BASIC_DF.hstack([pl.Series("result", [4, 4, 4])])
-        ),
-        pytest.param(
-            BASIC_DF,
-            dc.div_cols,
-            ["a", "a", "a"],
-            BASIC_DF.hstack([pl.Series("result", [1.0, 1.0, 1.0])]),
-        ),
-        pytest.param(
-            BASIC_DF,
-            dc.div_cols,
-            ["a", "b"],
-            BASIC_DF.hstack([pl.Series("result", [0.5, 0.5, 0.5])]),
-        ),
-        pytest.param(
-            BASIC_DF, dc.div_cols, ["b", "b"], BASIC_DF.hstack([pl.Series("result", [1, 1, 1])])
-        ),
+        pytest.param(BASIC_DF, dc.add_cols, ["a", "a", "a"], [3, 3, 3]),
+        pytest.param(BASIC_DF, dc.add_cols, ["a", "b"], [3, 3, 3]),
+        pytest.param(BASIC_DF, dc.add_cols, ["b", "b"], [4, 4, 4]),
+        pytest.param(BASIC_DF, dc.sub_cols, ["a", "a", "a"], [-1, -1, -1]),
+        pytest.param(BASIC_DF, dc.sub_cols, ["a", "b"], [-1, -1, -1]),
+        pytest.param(BASIC_DF, dc.sub_cols, ["b", "b"], [0, 0, 0]),
+        pytest.param(BASIC_DF, dc.mul_cols, ["a", "a", "a"], [1, 1, 1]),
+        pytest.param(BASIC_DF, dc.mul_cols, ["a", "b"], [2, 2, 2]),
+        pytest.param(BASIC_DF, dc.mul_cols, ["b", "b"], [4, 4, 4]),
+        pytest.param(BASIC_DF, dc.div_cols, ["a", "a", "a"], [1.0, 1.0, 1.0]),
+        pytest.param(BASIC_DF, dc.div_cols, ["a", "b"], [0.5, 0.5, 0.5]),
+        pytest.param(BASIC_DF, dc.div_cols, ["b", "b"], [1, 1, 1]),
     ],
 )
-def test_derive_cols(raw_data, derive_fn, cols, expected_result):
+def test_derive_cols(raw_data, derive_fn, cols, expected_series_values):
+    expected_result = raw_data.hstack([pl.Series("result", expected_series_values)])
     assert (
         raw_data.with_columns(derive_fn(cols).alias("result")).to_dicts()
         == expected_result.to_dicts()
